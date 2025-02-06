2b2edf1908ba223dc1f27cdef618090fb31893d0
David Lozano <davidalexisloz1@gmail.com>
2025-02-06 00:50:11 -0500

    :hammer_and_wrench: config: configure image domains and update package dependencies for authentication and data handling

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  }
+}
 export default nextConfig;
