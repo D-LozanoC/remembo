b9e01746b8a479f097c9b90d9ef2052056dab4c6
D-LozanoC <davidalexisloz1@gmail.com>
2025-07-07 15:21:37 -0500

    :sparkles: feat: enhance InputField component to accept custom className for styling

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,15 @@
 import type React from "react"
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string
+  label: string,
+  className?: string
 }
 
-export function InputField({ label, ...props }: InputFieldProps) {
+export function InputField({ label, className, ...props }: InputFieldProps) {
   return (
     <div>
       <input
-        className="mt-1 block w-full rounded-md border text-indigo-950 border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-200 focus:outline-none focus:ring-1 focus:ring-indigo-200 sm:text-sm"
+        className={`mt-1 block w-full rounded-md border text-indigo-950 border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-200 focus:outline-none focus:ring-1 focus:ring-indigo-200 sm:text-sm ${className}`}
         placeholder={label}
         {...props}
       />
