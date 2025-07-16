985ec35696a7f6317f77eb06e162d8b71935f35b
Laura Aponte <aldanielago@gmail.com>
2025-07-15 19:28:15 -0500

    :sparkles: feat: enhance LinksPage layout and add link category display

--- src/app/home/links/page.tsx
+++ src/app/home/links/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BsFillTrash2Fill as FillTrash } from "react-icons/bs"
+import { BsFillTrash2Fill as FillTrash, BsLink as LinkIcon } from "react-icons/bs"
 
 export default function LinksPage() {
   const links = [
@@ -17,16 +17,18 @@ export default function LinksPage() {
       description: 'Accede al código fuente y documentación del proyecto en GitHub.'
     }
   ]
+
   return (
-    <section className="pl-4 pt-4">
+    <section className="p-8 flex flex-col items-center">
       <h1 className="text-2xl font-bold text-gray-800">
         Links relativos al proyecto de grado
       </h1>
       {links.map((link, index) => (
-        <div key={index} className="mt-4 p-4 bg-white shadow rounded-lg max-w-2xl flex justify-between">
+        <div key={index} className="mt-4 p-4 bg-white shadow rounded-lg flex justify-between">
           <div>
             <h2 className="text-xl font-semibold text-gray-700">{link.title}</h2>
-            <p className="text-gray-600 mt-1">{link.description}</p>
+            <p className="text-gray-600 mt-1">Categoría: {link.category}</p>
+            <p className="text-gray-500 mt-1">{link.description}</p>
             <a
               href={link.url}
               className="text-blue-500 hover:underline mt-2 inline-block"
@@ -46,6 +48,10 @@ export default function LinksPage() {
         </div>
       )
       )}
+      <button className="mt-6 p-4 bg-transparent border border-white shadow rounded-lg max-w-2xl flex text-black hover:text-blue-700 cursor-pointer">
+        <LinkIcon className=" w-8 mt-1" />
+        <span>Agregar nuevo enlace</span>
+      </button>
     </section>
   )
 }
